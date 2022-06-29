1011b260c9bfa7c13297318bc25ba95546f7c84d
olkone <konecnyoliver@gmail.com>
2022-06-29 13:33:34 -0500

    Adjusts player selection to button press; updates DOM instead of console.log

---
+++ rps-ui.js
@@ -0,0 +1,99 @@
+
+const pScore = document.createElement('p');
+const cScore = document.createElement('p');
+const roundResult = document.createElement('p');
+
+function computerPlay() {
+
+    // Make an array of play choices (r=rock, p=paper, s=scissors)
+    const plays = ['r', 'p', 's'];
+
+    // Choose a random number in the length of the above array
+    const randomIndex = Math.floor(Math.random() * plays.length);
+    
+    // Index into the array with the random index number
+    return plays[randomIndex];
+}
+
+function playerSelection(e) {
+    return e.target.value;
+}
+
+
+// Initialize scores to 0 points each
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+    const status = document.querySelector('#status');
+    const player = document.querySelector('#player');
+    const computer = document.querySelector('#computer');
+    let result;
+
+    // Check through each possible choice combination
+    // Declare a round winner and allot a point
+    switch (playerSelection + computerSelection) {
+        default:
+            result = "Something went wrong.";
+            break;
+
+        case "rr":
+        case "pp":
+        case "ss":
+            result = "It's a draw!";
+            break;
+        
+        case "rp":
+            computerScore++;
+            result = "Paper beats rock. Computer wins!";
+            break;
+        
+        case "rs":
+            playerScore++;
+            result = "Rock beats scissors. Player wins!";
+            break;
+        
+        case "pr":
+            playerScore++;
+            result = "Paper beats rock. Player wins!";
+            break;
+        
+        case "ps":
+            computerScore++;
+            result = "Scissors beats paper. Computer wins!";
+            break;
+        
+        case "sr":
+            computerScore++;
+            result = "Rock beats scissors. Computer wins!";
+            break;
+
+        case "sp":
+            playerScore++;
+            result = "Scissors beats paper. Player wins!";
+            break;
+    }
+    
+    roundResult.textContent = result;
+    pScore.textContent = playerScore;
+    cScore.textContent = computerScore;
+
+    player.appendChild(pScore);
+    computer.appendChild(cScore);
+    status.appendChild(roundResult);
+}
+
+function game(e) {
+
+    // Set variables for play selection for player and computer
+    let playerSelection = e.target.value;
+    let computerSelection = computerPlay();
+
+    // Run playRound and show results
+    console.log(playRound(playerSelection, computerSelection));
+    
+
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', game));
